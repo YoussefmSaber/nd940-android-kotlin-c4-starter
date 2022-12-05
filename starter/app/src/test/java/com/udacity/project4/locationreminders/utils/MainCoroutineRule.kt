ca478b404bb2c7e5a991ca4da99808c84a7f9116
youssef mohammed <youssef.mu.saber@gmail.com>
2022-12-05 16:19:12 +0200

    Finished the project Add Test Cases

---
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/utils/MainCoroutineRule.kt
@@ -0,0 +1,37 @@
+package com.udacity.project4.locationreminders.utils
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+@ExperimentalCoroutinesApi
+class MainCoroutineRule(val dispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()) :
+    TestWatcher(),
+    TestCoroutineScope by TestCoroutineScope(dispatcher) {
+
+    /**
+     * Invoked when a test is about to start
+     * Sets the given dispatcher as an underlying dispatcher of Dispatchers.Main.
+     */
+
+    override fun starting(description: Description?) {
+        super.starting(description)
+        Dispatchers.setMain(dispatcher)
+    }
+
+    /**
+     * Invoked when a test method finishes (whether passing or failing)
+     * Resets state of the Dispatchers.Main to the original main dispatcher.
+     */
+
+    override fun finished(description: Description?) {
+        super.finished(description)
+        cleanupTestCoroutines()
+        Dispatchers.resetMain()
+    }
+}
